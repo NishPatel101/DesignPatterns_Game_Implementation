6690d73e375dd41caf1f97bf5d798c28747eb8dc
vincent <vtrej003@ucr.edu>
2020-06-07 23:15:55 -0700

    prototype ncurses Windows/read map file/print map file

--- test.cpp
+++ test.cpp
@@ -5,34 +5,47 @@
 #include <unistd.h>
 std::vector<std::string>map(100);
 std::vector<std::string> loadMap();
-void printArea();
+void printArea(WINDOW*);
 
 int main(int argc, char**argv) 
 {
   initscr();
+  start_color();
+  init_pair(1,COLOR_BLACK,COLOR_RED);
+  //attron(COLOR_PAIR(1));
   //printw("hello world");
-  refresh();
+  //attroff(COLOR_PAIR(1));
+  
+  //refresh();
+
   noecho();
   cbreak();
   //
   int yMax,xMax;
   getmaxyx(stdscr,yMax, xMax);
-  WINDOW * game = newwin(6,xMax,yMax,5);
+  WINDOW * leftWin = newwin(yMax/2,(xMax/2)-2,0,0);
+  WINDOW * rightWin = newwin(yMax/2,(xMax/2)-2,0,xMax/2);
+  wbkgd(leftWin, COLOR_PAIR(1));
+  //wbkgd(WindowName, COLOR_PAIR(1))
   refresh();
-  wrefresh(game);
-  //
+  wrefresh(leftWin);
+  wrefresh(rightWin);
+ 
   map = loadMap();
-  //
-  //for(int i = 0; i < map.size();i++)
-  //{
-  //    printw("%s\n", map[i].c_str());
-  //}
-  printArea();
+ 
+  
+  //wprintw(leftWin,"yMAX:%d",xMax);
+  
+  //wprintw(rightWin,"xMax:%d",yMax);
+  //wrefresh(leftWin);
+  //wrefresh(rightWin);
+  printArea(leftWin);
+  printArea(rightWin);
 
   refresh();
   sleep(4);
   int c = getch();
-  printw("%c",c);
+  //printw("%c",c);
   endwin();
 
   return 0;
@@ -50,12 +63,31 @@ std::vector<std::string> loadMap()
     return temp;
 }
 
-void printArea()
-{
-  for(int i = 0; i < 20; i++)
-  {
-      printw("%s\n", map[i].c_str());
-      refresh();
-  }
-  
+void printArea(WINDOW * win)
+{ 
+    int row, col;
+    char * c;
+    //mvwprintw(win,row+ 25,0,c);
+    for(row = 0; row < 20; row++)
+    {
+	//wprintw(win, "%c", map[0].at(0));
+
+	for(col = 0; col < 10; col++)
+	{
+	    &c = map[row].at(col);
+            mvwprintw(win, row, col, "%c", *c);
+
+	    /*if( == 0)
+	    {
+	     	  mvwprintw(win,row, col, " ");
+	    }
+	    else
+	    {
+		mvwprintw(win,row, col,"%s", c);    
+                //wrefresh(win);
+	    }*/
+	}
+
+    }
+    wrefresh(win);  
 }
