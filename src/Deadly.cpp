b6047d22234fa899c4e2e60ccc066d69f557555b
NishPatel101 <57877220+NishPatel101@users.noreply.github.com>
2020-06-08 16:47:08 -0700

    Separated Deadly class into two files

---
+++ src/Deadly.cpp
@@ -0,0 +1,17 @@
+#include "..header/Deadly.h"
+
+using namespace std;
+
+Deadly::Deadly(Equipment* obj) : EquipmentDecorator(obj) {}
+
+string Deadly::getDescription() {
+		return "Deadly " + e->getDescription() + "\nThis weapon is deadly, so it has a lot more attack power.";
+}
+
+int Deadly::getDefense() {
+		return 0; //Not implemented yet
+}
+
+int Deadly::getDamage() {
+		return e->getDamage() + 5;
+}
