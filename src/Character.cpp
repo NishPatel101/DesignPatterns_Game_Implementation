8334bea319da66d79bf18287afc3cf18fac06af5
Nish Patel <nishpatel101@yahoo.com>
2020-06-08 16:00:44 -0700

    Added Character class

---
+++ src/Character.cpp
@@ -0,0 +1,38 @@
+#include "..header/Character.hpp"
+
+using namespace std;
+
+Character::Character(string _name, int _health, Equipment* _e) {
+	name = _name;
+	health = _health;
+	e = _e;
+}
+
+string Character::getName() {
+	return name;
+}
+
+void Character::setName(string n) {
+	name = n;
+}
+
+int Character::getHealth() {
+	return health;
+}
+
+void Character::setHealth(int h) {
+	health = h;
+}
+
+int Character::getDamage() {
+	return e->getDamage();
+}
+
+void Character::takeDamage(int d) {
+	healh -= d;
+}
+
+void Character::addEquipment(Equipment* eq) {
+	delete e;
+	e = eq;
+}
