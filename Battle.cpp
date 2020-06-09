ff2de3c8506eb1052c0ae1e541f5f34f682db2ab
Ricardo <63069526+rlira004@users.noreply.github.com>
2020-06-08 19:15:38 -0700

    Update Battle.cpp

--- Battle.cpp
+++ Battle.cpp
@@ -2,13 +2,10 @@
 
         /* Constructors */
 Battle::Battle() { }
-Battle::Battle()(Character* player, Character* enemy) {
-        //logic go here;
-}
         
 //I don't think we need these?        
 //	/* Variables */
 //        Battle::getHealth();
 //        Battle::getDamage();
 //  /* Pure Virtual Functions */
-        virtual void battle(Character* player, Character* enemy) = 0;
+        virtual void doBattle(Character* player, Character* enemy) = 0;
