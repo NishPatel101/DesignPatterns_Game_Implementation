0860fab353674dace0bf1090d2d0eef763498819
Ricardo <63069526+rlira004@users.noreply.github.com>
2020-06-08 18:37:32 -0700

    Create Battle.cpp

---
+++ Battle.cpp
@@ -0,0 +1,20 @@
+#ifndef __BATTLE_CPP__
+#define __BATTLE_CPP__
+
+
+#include "Battle.h"
+
+        /* Constructors */
+Battle::Battle() { }
+Battle::Battle()(Character* player, Character* enemy) {
+        //logic go here;
+}
+        
+//I don't think we need these?        
+//	/* Variables */
+//        Battle::getHealth();
+//        Battle::getDamage();
+//  /* Pure Virtual Functions */
+        virtual void battle(Character* player, Character* enemy) = 0;
+
+#endif //__BATTLE_CPP__
