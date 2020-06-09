bf78940602198409dedf39b5688d7b179841399b
Ricardo <63069526+rlira004@users.noreply.github.com>
2020-06-08 22:23:28 -0700

    Update and rename UserInput.cpp to BattleVisitor.h

---
+++ BattleVisitor.h
@@ -0,0 +1,11 @@
+//Battle.h
+//#include "Character.h"
+
+class doBattle {
+        public:
+    getHP();
+    userInput();
+    doDamage();
+    runAway();
+    doBattle(Character* Player, Character* Enemy);
+};
