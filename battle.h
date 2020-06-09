2bcbc05c4d8f70480d4240b52444975ec99d5e69
Ricardo <63069526+rlira004@users.noreply.github.com>
2020-06-08 17:10:03 -0700

    Create battle.h

---
+++ battle.h
@@ -0,0 +1,31 @@
+#ifndef __BATTLE_H__
+#define __BATTLE_H__
+
+//#include "Equipment.h"
+//#include "Character.h"
+
+/*
+Character:
+priv:
+string name
+int health
+
+prot:
+equipment* e;
+*/
+
+
+class Battle {    
+    public:
+        /* Constructors */
+        Battle();
+        
+        /* Variables */
+        getHealth();
+        getDamage();
+        
+        /* Pure Virtual Functions */
+        virtual void battle(Character* player, Character* enemy) = 0;
+};
+
+#endif //__BATTLE_H__
